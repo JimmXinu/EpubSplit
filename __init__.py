0d3d1b828cd2e38d84796553a63fc2e382c210e7
Jim Miller <retiefjimm@gmail.com>
2015-05-12 11:58:22 -0500

    Bump version to 2.1.0.

--- __init__.py
+++ __init__.py
@@ -42,7 +42,7 @@ class EpubSplitBase(InterfaceActionBase):
     description         = _('UI plugin to split off parts of an epub into a new book.')
     supported_platforms = ['windows', 'osx', 'linux']
     author              = 'Jim Miller'
-    version             = (2, 0, 3)
+    version             = (2, 1, 0)
     minimum_calibre_version = (0, 9, 29)
 
     #: This field defines the GUI plugin class that contains all the code
