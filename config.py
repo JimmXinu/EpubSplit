9325efc84afcce864a6d35c8e59c4dbac414561c
Jim Miller <retiefjimm@gmail.com>
2018-12-25 08:54:22 -0600

    Force Translation Update

--- config.py
+++ config.py
@@ -47,8 +47,6 @@ from calibre_plugins.epubsplit.common_utils \
 PREFS_NAMESPACE = 'EpubSplitPlugin'
 PREFS_KEY_SETTINGS = 'settings'
 
-TRANS_TEMP = _('Translation')
-
 # Set defaults used by all.  Library specific settings continue to
 # take from here.
 default_prefs = {}
